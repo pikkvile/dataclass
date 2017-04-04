99ed088140ac774e593c444a19005094218a8c08
Ilya Dubov <mail@mail.ru>
2017-04-05 01:34:48 +0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+const DataClass = angular.module('DataClass', []);
+
+DataClass.controller('IndexCtrl', ($scope, parser) => {
+    $scope.src = '';
+    $scope.parse = () => {
+        $scope.target = parser.parse($scope.src);
+    };
+});
+
+DataClass.service('parser', function() {
+
+    this.parse = (src) => {
+        var classes = src.split(';');
+        return classes.filter(clsSrc => !!clsSrc).map((clsSrc) => {
+            clsSrc = clsSrc.trim();
+            return {
+                className: clsSrc.split(':')[0].trim(),
+				fields: parseFields(clsSrc.split(':')[1].trim())
+            };
+        });
+    };
+	
+	function parseFields(fSrc) {
+		return fSrc.split(',')
+			.map(fsrc => fsrc.trim())
+			.map(fsrc => {
+				const fArray = fsrc.split(' ');
+				return {
+					type: fArray.length == 2 ? fArray[0].trim() : 'String',
+					name: fArray[fArray.length - 1]
+				};
+			});
+	}
+});
