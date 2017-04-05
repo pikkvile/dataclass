d0ba3a06eec87f87cdf1d1f0da124269f7b73406
Ilya Dubov <pikkvile@gmail.com>
2017-04-05 17:33:06 +0300

    it works

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 const DataClass = angular.module('DataClass', []);
 
 DataClass.controller('IndexCtrl', ($scope, parser) => {
-    $scope.src = '';
+    $scope.src = 'Test: int i, title;\nNotification: to, from, long sentAt, NotificationDeliveryChannel deliveryChannel, body;';
     $scope.parse = () => {
         $scope.target = parser.parse($scope.src);
     };
+    $scope.parse();
 });
 
 DataClass.service('parser', function() {
@@ -15,20 +16,23 @@ DataClass.service('parser', function() {
             clsSrc = clsSrc.trim();
             return {
                 className: clsSrc.split(':')[0].trim(),
-				fields: parseFields(clsSrc.split(':')[1].trim())
+                fields: parseFields(clsSrc.split(':')[1].trim())
             };
         });
     };
-	
-	function parseFields(fSrc) {
-		return fSrc.split(',')
-			.map(fsrc => fsrc.trim())
-			.map(fsrc => {
-				const fArray = fsrc.split(' ');
-				return {
-					type: fArray.length == 2 ? fArray[0].trim() : 'String',
-					name: fArray[fArray.length - 1]
-				};
-			});
-	}
+    
+    function parseFields(fSrc) {
+        return fSrc.split(',')
+            .map(fsrc => fsrc.trim())
+            .map(fsrc => {
+                const fArray = fsrc.split(' ');
+                return {
+                    type: fArray.length == 2 ? fArray[0].trim() : 'String',
+                    name: fArray[fArray.length - 1],
+                    gsName: capitalize(fArray[fArray.length - 1])
+                };
+            });
+    }
+
+    const capitalize = ([first,...rest]) => first.toUpperCase() + rest.join('').toLowerCase();
 });
